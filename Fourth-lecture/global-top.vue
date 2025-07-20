6cc9cd1206a849ff943a3ded759e750c118bade8
goudanZ1 <1390928710@qq.com>
2025-07-20 21:53:11 +0800

    docs: upload the fourth lecture (React)

---
+++ Fourth-lecture/global-top.vue
@@ -0,0 +1,11 @@
+<template>
+  <Pagination
+    v-if="
+      $slidev.nav.currentPage !== $slidev.nav.total + 1 &&
+      !$slidev.configs.paginationConfig.paginationPagesDisabled?.includes($slidev.nav.currentPage) &&
+      ($slidev.configs.paginationConfig.paginationX || $slidev.configs.paginationConfig.paginationY)
+    "
+    :x="$slidev.configs.paginationConfig.paginationX"
+    :y="$slidev.configs.paginationConfig.paginationY"
+  />
+</template>
